8506c05fd5503d28699a203c8930242fa2c1a474
Justine Pic <justine.pic@outlook.fr>
2021-04-27 13:07:57 +0200

    Commentaire

---
+++ Exercice-21/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char couleur;
+            do
+            {
+                // Saisir une couleur
+                Console.Write("Saisir une couleur (V/O/R) = ");
+                couleur = Console.ReadKey().KeyChar;
+                Console.ReadLine();
+            } while (couleur != 'O' && couleur != 'R' && couleur != 'V');
+
+            // Différents ordre à exécuter
+            switch (couleur)
+            {
+                    case 'V':
+                        Console.WriteLine("Passer");
+                        break;
+                    case 'O':
+                        Console.WriteLine("Ralentir");
+                        break;
+                    case 'R':
+                        Console.WriteLine("S'arrêter");
+                        break;
+
+            } Console.ReadLine();
+            
+
+
+
+
+        } 
+        
+            
+            
+    }
+}
